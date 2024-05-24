a8f1b3b95804b3747511e0530a81da4c4f03e332
RANJITH7305141 <ranjithkumarmrk016@outlook.com>
2024-05-24 15:39:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
 
 app=Flask(__name__)
 
+try:
+    app.config["MONGO_URI"] = os.getenv('mongo_url')
+    client=MongoClient(os.getenv('mongo_url'))
+    db=client['Wheels4rent']
+except:
+    pass
 
 @app.route('/')
 def home():
-    return "welcome"
-
+    try:
+        return render_template('register.html')
+    except:
+        return "not working"
 
 if __name__ =="__main__":
     app.run(debug=True)
