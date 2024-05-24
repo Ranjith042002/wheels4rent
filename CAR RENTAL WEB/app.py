b640c8f46282186eb9b67343166889e8d7e4946e
RANJITH7305141 <ranjithkumarmrk016@outlook.com>
2024-05-24 14:50:56 +0530

    Update app.py

--- CAR RENTAL WEB/app.py
+++ CAR RENTAL WEB/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-from flask_cors import CORS
+# from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return "Hello"
+    try:
+        return render_template('register.html')
+    except:
+        return "Not Working"
 
 if __name__ =="__main__":
     app.run(debug=True)
