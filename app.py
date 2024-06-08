9c9735c6d70420ac9b05e3a33f2423dbdae525e0
RANJITH7305141 <ranjithkumarmrk016@outlook.com>
2024-06-09 01:29:18 +0530

    review

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app=Flask(__name__)
 app.config["MONGO_URI"] = os.getenv('mongo_url')
 client=MongoClient(os.getenv('mongo_url'))
 db=client['Wheels4rent']
+reviews_collection = db['reviews']
+
 
 @app.route('/')
 def home():
@@ -94,9 +96,8 @@ def ride():
 
     return render_template('ride.html')
 
-@app.route('/reviews', methods=['POST','GET'])
+@app.route('/reviews', methods=['GET','POST'])
 def submit_review():
-    try:
         if request.method == 'POST':
             name = request.form['name']
             email = request.form['email']
@@ -104,13 +105,12 @@ def submit_review():
 
             # Insert the new review into the database
             new_review = {'name': name, 'email': email, 'review': review}
-            db.review.insert_one(new_review)
+            db.reviews.insert_one(new_review)
 
-            # Retrieve all reviews from the MongoDB collection
-            reviews = list(db.review.find())
+        # Retrieve all reviews from the MongoDB collection
+        reviews = list(reviews_collection.find())
         return render_template('reviews.html', reviews=reviews)
-    except:
-        return render_template('reviews.html')
+
 
 
 
